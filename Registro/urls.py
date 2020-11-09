e617dda9cf93acd58c00f3997adf698f957e25d0
Fernando Caris <fe.caris@alumnos.duoc.cl>
2020-11-09 20:13:42 -0300

    diseñando formularios

--- Registro/urls.py
+++ Registro/urls.py
@@ -4,7 +4,15 @@ from . import views
 urlpatterns = [
     path('',views.menu),
     path('listaUsuarios',views.Usuario_list),
+
+    path('', views.Usuario_list, name='usuario_list'),
+    path('usuario/<int:pk>/', views.usuario_detail, name='usuario_detail'),
+
+    path('usuario/new/',views.usuario_new, name='usuario_new'),
+    #path('usuario/<int:pk>/new/',views.usuario_edit, name='usuario_edit'),
+
+
     path('top2020',views.top2020),
     path('top2019',views.top2019),
-    path('noticias',views.noticias)
+    path('noticias',views.noticias),    
 ]
--- Registro/views.py
+++ Registro/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from .models import Usuario
+from .forms import UsuarioForm
+from django.shortcuts import render, get_object_or_404
+from django.utils import timezone
+from django.shortcuts import redirect
 
 # Create your views here.
 def Usuario_list(request):
     usuarios = Usuario.objects.all()
-    return render(request, 'Registro/Usuario_list.html', {'lista_Usuarios': usuarios})
+    #antes era Registro/Usuario_list.html'
+    return render(request, 'Registro/Usuario_list_ext.html', {'lista_Usuarios': usuarios})
 
 def menu(request):
     return render(request, 'Registro/menu.html',{})
@@ -17,3 +22,29 @@ def top2019(request):
 
 def noticias(request):
     return render(request, 'Registro/noticias.html',{})
+
+
+#USUARIO
+def usuario_detail(request, pk):
+    usuario = get_object_or_404(Usuario, pk=pk)
+    return render(request, 'Registro/usuario_detail.html', {'usuario': usuario})
+
+#crear nuevo usuario
+def usuario_new(request):
+    form = UsuarioForm()
+    return render(request, 'Registro/registroUsuario.html',{'form': form})
+
+#esto aun no esta listo 
+def usuario_edit(request, pk):
+    usuario = get_object_or_404(Usuario, pk=pk)
+    if request.method == "POST":
+        form = UsuarioForm(request.POST, instance=usuario)
+        if usuario.is_valid():
+            usuario = form.save(commit=False)
+            usuario.author = request.user
+            usuario.published_date = timezone.now()
+            usuario.save()
+            return redirect('usuario_detail', pk=usuario.pk)
+    else:
+        form = UsuarioForm(instance=usuario)
+    return render(request, 'Registro/usuario_edit.html', {'form': form})
